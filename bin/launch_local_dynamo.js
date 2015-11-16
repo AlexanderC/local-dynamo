7d5c721466f58c1b268fa04e587391bc65fa4f2e
AlexanderC <alexander.moldova@gmail.com>
2015-11-16 15:26:09 +0200

    Tweak lounch by adding pid files

--- bin/launch_local_dynamo.js
+++ bin/launch_local_dynamo.js
@@ -12,26 +12,31 @@ flags.defineString('database_dir', '', 'The location for databases files. Run in
 flags.defineNumber('port', 4567, 'A port to run DynamoDB Local');
 flags.parse();
 
-var childProcess = localDynamo.launch({
+try {
+  var childProcess = localDynamo.launch({
     dir: flags.get('database_dir') || null,
     stdio: 'pipe',
-}, flags.get('port'));
+  }, flags.get('port'));
 
-childProcess.stdout.pipe(process.stdout);
-childProcess.stderr.pipe(process.stderr);
+  childProcess.stdout.pipe(process.stdout);
+  childProcess.stderr.pipe(process.stderr);
 
-childProcess.on('error', function (e) {
-  if (e.code === 'ENOENT') {
-  	console.error('Failed to start DynamoDB Local. Maybe because the database directory does not exist.');
-  } else {
-  	console.error('Failed to start DynamoDB Local. Error:', e.message);
-  }
-});
+  childProcess.on('error', function (e) {
+    if (e.code === 'ENOENT') {
+      console.error('Failed to start DynamoDB Local. Maybe because the database directory does not exist.');
+    } else {
+      console.error('Failed to start DynamoDB Local. Error:', e.message);
+    }
+  });
 
-childProcess.stdout.on('data', function(data) {
-  if (data.toString() && !hasStartedMsg) {
-    hasStartedMsg = true;
+  childProcess.stdout.on('data', function(data) {
+    if (data.toString() && !hasStartedMsg) {
+      hasStartedMsg = true;
 
-    console.log('Local DynamoDB has successfully started!');
-  }
-});
+      console.log('Local DynamoDB has successfully started!');
+    }
+  });
+} catch(error) {
+  console.error(error.toString());
+  process.exit(1);
+}
