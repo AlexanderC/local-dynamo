e8a8b45c41c107f9590d4540074344bc3687e702
AlexanderC <alexander.moldova@gmail.com>
2015-11-16 11:45:01 +0200

    Small improvements

--- bin/launch_local_dynamo.js
+++ bin/launch_local_dynamo.js
@@ -4,25 +4,25 @@
  * @fileoverview The script to launch DynamoDB Local from command line.
  */
 
-var flags = require('flags')
-var localDynamo = require('../lib/launch.js')
+var flags = require('flags');
+var localDynamo = require('../lib/launch.js');
 
-flags.defineString('database_dir', '', 'The location for databases files. Run in memory if omitted.')
-flags.defineNumber('port', 4567, 'A port to run DynamoDB Local')
-flags.parse()
+flags.defineString('database_dir', '', 'The location for databases files. Run in memory if omitted.');
+flags.defineNumber('port', 4567, 'A port to run DynamoDB Local');
+flags.parse();
 
 var childProcess = localDynamo.launch({
     dir: flags.get('database_dir') || null,
     stdio: 'pipe',
-}, flags.get('port'))
+}, flags.get('port'));
 
-childProcess.stdout.pipe(process.stdout)
-childProcess.stderr.pipe(process.stderr)
+childProcess.stdout.pipe(process.stdout);
+childProcess.stderr.pipe(process.stderr);
 
 childProcess.on('error', function (e) {
   if (e.code === 'ENOENT') {
-  	console.error('Failed to start DynamoDB Local. Maybe because the database directory does not exist.')
+  	console.error('Failed to start DynamoDB Local. Maybe because the database directory does not exist.');
   } else {
-  	console.error('Failed to start DynamoDB Local. Error:', e.message)
+  	console.error('Failed to start DynamoDB Local. Error:', e.message);
   }
-})
+});
